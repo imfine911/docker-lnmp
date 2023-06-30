79649a6ada5cc3cc421f56c9508b6a9da7e92ca6
ruiqiangqiang <10612737@qq.com>
2023-06-30 16:14:00 +0800

    feat 启动web服务sh  composer sh

---
+++ sh/composer.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# shellcheck disable=SC2046
+path=$(cd $(dirname -- "$0") && pwd )
+echo  "$path"
+
+# shellcheck disable=SC2039
+array=(${path//// })
+
+# shellcheck disable=SC2068
+echo ${array[@]: -1}
+
+# shellcheck disable=SC2145
+docker exec php8.1 /bin/sh -c "cd /var/www/${array[@]: -1} && composer $1"
+
+
